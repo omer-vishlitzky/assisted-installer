b27737cb0db03d8b88a0688a370bc298a431adb1
omer vishlitzky <omer.vishlitzky@gmail.com>
2025-08-12 14:03:02 +0300

    ACM-21665: CVE-2025-30204 Update dependencies to fix CVE-2025-30204

--- vendor/gopkg.in/square/go-jose.v2/encoding.go
+++ vendor/gopkg.in/square/go-jose.v2/encoding.go
@@ -21,13 +21,12 @@ import (
 	"compress/flate"
 	"encoding/base64"
 	"encoding/binary"
-	"fmt"
 	"io"
 	"math/big"
 	"strings"
 	"unicode"
 
-	"gopkg.in/go-jose/go-jose.v2/json"
+	"gopkg.in/square/go-jose.v2/json"
 )
 
 // Helper function to serialize known-good objects.
@@ -42,7 +41,7 @@ func mustSerializeJSON(value interface{}) []byte {
 	// MarshalJSON will happily serialize it as the top-level value "null". If
 	// that value is then embedded in another operation, for instance by being
 	// base64-encoded and fed as input to a signing algorithm
-	// (https://github.com/go-jose/go-jose/issues/22), the result will be
+	// (https://github.com/square/go-jose/issues/22), the result will be
 	// incorrect. Because this method is intended for known-good objects, and a nil
 	// pointer is not a known-good object, we are free to panic in this case.
 	// Note: It's not possible to directly check whether the data pointed at by an
@@ -86,7 +85,7 @@ func decompress(algorithm CompressionAlgorithm, input []byte) ([]byte, error) {
 	}
 }
 
-// deflate compresses the input.
+// Compress with DEFLATE
 func deflate(input []byte) ([]byte, error) {
 	output := new(bytes.Buffer)
 
@@ -98,26 +97,14 @@ func deflate(input []byte) ([]byte, error) {
 	return output.Bytes(), err
 }
 
-// inflate decompresses the input.
-//
-// Errors if the decompressed data would be >250kB or >10x the size of the
-// compressed data, whichever is larger.
+// Decompress with DEFLATE
 func inflate(input []byte) ([]byte, error) {
 	output := new(bytes.Buffer)
 	reader := flate.NewReader(bytes.NewBuffer(input))
 
-	maxCompressedSize := 10 * int64(len(input))
-	if maxCompressedSize < 250000 {
-		maxCompressedSize = 250000
-	}
-
-	limit := maxCompressedSize + 1
-	n, err := io.CopyN(output, reader, limit)
-	if err != nil && err != io.EOF {
+	_, err := io.Copy(output, reader)
+	if err != nil {
 		return nil, err
-	}
-	if n == limit {
-		return nil, fmt.Errorf("uncompressed data would be too large (>%d bytes)", maxCompressedSize)
 	}
 
 	err = reader.Close()
@@ -140,7 +127,7 @@ func newBuffer(data []byte) *byteBuffer {
 
 func newFixedSizeBuffer(data []byte, length int) *byteBuffer {
 	if len(data) > length {
-		panic("go-jose/go-jose: invalid call to newFixedSizeBuffer (len(data) > length)")
+		panic("square/go-jose: invalid call to newFixedSizeBuffer (len(data) > length)")
 	}
 	pad := make([]byte, length-len(data))
 	return newBuffer(append(pad, data...))
